bbc8a13d4a71fba5786da7d0104f969178f526f5
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-05-11 01:01:44 +0530

    feat: add theme

---
+++ src/theme/theme.jsx
@@ -0,0 +1,26 @@
+const theme = {
+    background: {
+      primary: "#E5E5E5",
+      secondary: "#FFFFFF",
+      blue: "#12B8FF",
+      lightBlue: "#DBF4FF",
+    },
+    color: {
+      primary: "#3C4858",
+      secondary: "#8492A6",
+      blue: "#12B8FF",
+      white: "#ffffff",
+      darkGray: "#3C4858",
+    },
+    input: {
+      primary: "#F0F2F4",
+    },
+    button: {
+      primary: "#12B8FF",
+    },
+    border: {
+      primary: "#12B8FF",
+    },
+  };
+  
+  export { theme };
